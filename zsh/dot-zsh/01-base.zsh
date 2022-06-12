2a932f15eee5abdf50ef838fcbea197231fa8504
Jason Raimondi <jason@raimondi.us>
2022-06-11 22:30:54 -0700

    feat: add prepend_path and append_path helpers

--- zsh/dot-zsh/01-base.zsh
+++ zsh/dot-zsh/01-base.zsh
@@ -1,5 +1,23 @@
 # add timestamps to history
 setopt EXTENDED_HISTORY
 
+function prepend_path() {
+  [[ ! -d "$1" ]] && return
+
+  path=(
+      $1
+      $path
+  )
+}
+
+function append_path() {
+  [[ ! -d "$1" ]] && return
+
+  path=(
+      $path
+      $1
+  )
+}
+
 # add bin repository
-test -e $DOTFILES_HOME/bin && PATH="$PATH:$DOTFILES_HOME/bin"
+test -e $DOTFILES_HOME/bin && prepend_path "$DOTFILES_HOME/bin"
--- zsh/dot-zsh/04-programming.zsh
+++ zsh/dot-zsh/04-programming.zsh
@@ -17,16 +17,16 @@ fi
 
 # Golang
 if test -e $HOME/Code/go; then
-    export GOPATH=${HOME}/Code/go; 
     export GO111MODULE=on; 
-    export PATH=$PATH:/usr/local/go/bin
+    export GOPATH="$HOME/Code/go"; 
+    prepend_path "/usr/local/go/bin"
 fi
 
 # K8s
-test -e $HOME/.krew/bin && export PATH="$HOME/.krew/bin:$PATH";
+test -e $HOME/.krew/bin && append_path "$HOME/.krew/bin";
 
 # Minio
 test -e /usr/local/bin/mc && complete -o nospace -C /usr/local/bin/mc mc;
 
 # iTerm2
-test -e $HOME/.iterm2_shell_integration.zsh && source "${HOME}/.iterm2_shell_integration.zsh"
+test -e $HOME/.iterm2_shell_integration.zsh && source "$HOME/.iterm2_shell_integration.zsh"
