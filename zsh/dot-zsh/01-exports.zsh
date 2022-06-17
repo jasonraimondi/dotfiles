d56ebaf6b1350aa914e8e5cf1fd6246a09f82fa5
Jason Raimondi <jason@raimondi.us>
2022-06-16 21:14:29 -0700

    feat: add and organize exports

--- zsh/dot-zsh/01-exports.zsh
+++ zsh/dot-zsh/01-exports.zsh
@@ -1,6 +1,10 @@
-export GPG_TTY=$(tty)
+# Avoid issues with `gpg` as installed via Homebrew.
+# https://stackoverflow.com/a/42265848/96656
+export GPG_TTY=$(tty);
+
 export EDITOR='nvim'
 export GIT_EDITOR='nvim'
+export GUI_EDITOR='subl'
 
 # homebrew stop analytics
 export HOMEBREW_NO_ANALYTICS=1
@@ -16,3 +20,14 @@ export DOCKER_BUILDKIT=1
 
 # display how long all tasks over 10 seconds take
 export REPORTTIME=10
+
+
+# Enable persistent REPL history for `node`.
+export NODE_REPL_HISTORY=~/.node_history;
+# Allow 32³ entries; the default is 1000.
+export NODE_REPL_HISTORY_SIZE='32768';
+# Use sloppy mode by default, matching web browsers.
+export NODE_REPL_MODE='sloppy';
+
+# Don’t clear the screen after quitting a manual page.
+export MANPAGER='less -X';
