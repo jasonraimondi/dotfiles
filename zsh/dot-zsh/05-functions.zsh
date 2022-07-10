6b9915270b418aefec9996c2c29299629dfbd9cf
Jason Raimondi <jason@raimondi.us>
2022-07-09 23:53:47 -0400

    chore: formatting

--- zsh/dot-zsh/05-functions.zsh
+++ zsh/dot-zsh/05-functions.zsh
@@ -1,21 +1,22 @@
 # open github aka ogh
 function ogh() {
-	local REMOTE_NAME="$(git remote -v | awk '{print $2}' | head -n 1)"
-	if [[ $REMOTE_NAME == *"github"* ]]; then
-		local URL="https://github.com/${REMOTE_NAME:15:-4}"
-		echo "Opening $URL"
-	  open "$URL"
-	else
-		echo "This doesnt look like a github repository"
-	fi
+  local REMOTE_NAME="$(git remote -v | awk '{print $2}' | head -n 1)"
+
+  if [[ $REMOTE_NAME == *"github"* ]]; then
+    local URL="https://github.com/${REMOTE_NAME:15:-4}"
+    echo "Opening $URL"
+    # open "$URL"
+  else
+    echo "This doesnt look like a github repository"
+  fi
 }
 
 function e() {
-	if [ "$1" = "" ] ; then
-	  exec $EDITOR .
-	else
-	  exec $EDITOR "$1"
-	fi
+  if [ "$1" = "" ] ; then
+    exec $EDITOR .
+  else
+    exec $EDITOR "$1"
+  fi
 }
 
 function latest() {
@@ -30,7 +31,7 @@ function versions() {
 }
 
 function nname () {
-	deno run -q --allow-read $HOME/dotfiles/misc/namer/namer.ts
+  deno run -q --allow-read $HOME/dotfiles/lib/namer/namer.ts
 }
 
 function fname() { 
@@ -72,12 +73,12 @@ function slugify() {
 }
 
 function mini-img() {
-	local RESIZE=""
-	if [ test -e $2 ]; then
-		local RESIZE="-resize $2x"
-	fi
-	convert -strip -interlace Plane -gaussian-blur 0.05 "$RESIZE" -quality 85% "$1" "$1.min.jpg"
-	exit 0
+  local RESIZE=""
+  if [ test -e $2 ]; then
+    local RESIZE="-resize $2x"
+  fi
+  convert -strip -interlace Plane -gaussian-blur 0.05 "$RESIZE" -quality 85% "$1" "$1.min.jpg"
+  exit 0
 }
 
 function fluxsync() {
@@ -106,44 +107,50 @@ function fluxsync() {
 # Show all the names (CNs and SANs) listed in the SSL certificate
 # for a given domain
 function getcertnames() {
-	if [ -z "${1}" ]; then
-		echo "ERROR: No domain specified.";
-		return 1;
-	fi;
+  if [ -z "${1}" ]; then
+    echo "ERROR: No domain specified.";
+    return 1;
+  fi;
 
-	local domain="${1}";
-	echo "Testing ${domain}…";
-	echo ""; # newline
+  local domain="${1}";
+  echo "Testing ${domain}…";
+  echo ""; # newline
 
-	local tmp=$(echo -e "GET / HTTP/1.0\nEOT" \
-		| openssl s_client -connect "${domain}:443" -servername "${domain}" 2>&1);
+  local tmp=$(echo -e "GET / HTTP/1.0\nEOT" \
+    | openssl s_client -connect "${domain}:443" -servername "${domain}" 2>&1);
 
-	if [[ "${tmp}" = *"-----BEGIN CERTIFICATE-----"* ]]; then
-		local certText=$(echo "${tmp}" \
-			| openssl x509 -text -certopt "no_aux, no_header, no_issuer, no_pubkey, \
-			no_serial, no_sigdump, no_signame, no_validity, no_version");
-		echo "Common Name:";
-		echo ""; # newline
-		echo "${certText}" | grep "Subject:" | sed -e "s/^.*CN=//" | sed -e "s/\/emailAddress=.*//";
-		echo ""; # newline
-		echo "Subject Alternative Name(s):";
-		echo ""; # newline
-		echo "${certText}" | grep -A 1 "Subject Alternative Name:" \
-			| sed -e "2s/DNS://g" -e "s/ //g" | tr "," "\n" | tail -n +2;
-		return 0;
-	else
-		echo "ERROR: Certificate not found.";
-		return 1;
-	fi;
+  if [[ "${tmp}" = *"-----BEGIN CERTIFICATE-----"* ]]; then
+    local certText=$(echo "${tmp}" \
+      | openssl x509 -text -certopt "no_aux, no_header, no_issuer, no_pubkey, \
+      no_serial, no_sigdump, no_signame, no_validity, no_version");
+    echo "Common Name:";
+    echo ""; # newline
+    echo "${certText}" | grep "Subject:" | sed -e "s/^.*CN=//" | sed -e "s/\/emailAddress=.*//";
+    echo ""; # newline
+    echo "Subject Alternative Name(s):";
+    echo ""; # newline
+    echo "${certText}" | grep -A 1 "Subject Alternative Name:" \
+      | sed -e "2s/DNS://g" -e "s/ //g" | tr "," "\n" | tail -n +2;
+    return 0;
+  else
+    echo "ERROR: Certificate not found.";
+    return 1;
+  fi;
 }
 
 function sidebyside() {
-	local OUTPUT="${3:-ouput.png}"
-	if [ -z "${2}" ]; then
-		echo "requires two inputs";
-		return 1;
-	fi;
-	montage "$1" "$2" -tile 2x1 -geometry +20+20 -background none "$OUTPUT"
+  local OUTPUT="${3:-ouput.png}"
+  if [ -z "${2}" ]; then
+    echo "requires two inputs";
+    return 1;
+  fi;
+  montage "$1" "$2" -tile 2x1 -geometry +20+20 -background none "$OUTPUT"
+}
+
+# Run `dig` and display the most useful info
+function digga() {
+  dig +nocmd "$1" any +multiline +noall +answer;
+}
 
 function targz() {
   local tmpFile="${@%/}.tar";
