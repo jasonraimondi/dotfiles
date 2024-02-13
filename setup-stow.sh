3b4af17430e4624efce51a496293809d58e0cb65
Jason Raimondi <jason@raimondi.us>
2024-02-12 23:13:59 -0500

    reset: add mise.toml

--- setup-stow.sh
+++ setup-stow.sh
@@ -6,7 +6,7 @@ stow -v -R --dotfiles git
 stow -v -R --dotfiles iterm2
 stow -v -t ~/Library/Spelling dictionary
 stow -v -R --dotfiles mackup
-stow -v -R --dotfiles mise
+stow -v --dotfiles mise
 stow -v -R --dotfiles ruby
 stow -v -t ~/.ssh ssh
 stow -v -R --dotfiles tmux
