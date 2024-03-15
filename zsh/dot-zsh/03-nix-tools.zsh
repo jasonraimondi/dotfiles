64af3470cc28155e31dbd9aa81af075a6a539451
Jason Raimondi <jason@raimondi.us>
2024-03-15 17:07:47 -0400

    fix: nix tools shell command

--- zsh/dot-zsh/03-nix-tools.zsh
+++ zsh/dot-zsh/03-nix-tools.zsh
@@ -1,25 +1,25 @@
 
 function asciinema() {
-  nix-shell -p asciinema --run "asciinema $@"
+  nix-shell -p asciinema --run "asciinema $*"
 }
 function dive() {
-  nix-shell -p dive --run "dive $@"
+  nix-shell -p dive --run "dive $*"
 }
 function heroku() {
-  nix-shell -p heroku --run "heroku $@"
+  nix-shell -p heroku --run "heroku $*"
 }
 function masscan() {
-  nix-shell -p masscan --run "masscan $@"
+  nix-shell -p masscan --run "masscan $*"
 }
 function overmind() {
-  nix-shell -p overmind --run "overmind $@"
+  nix-shell -p overmind --run "overmind $*"
 }
 function restic() {
-  nix-shell -p restic --run "restic $@"
+  nix-shell -p restic --run "restic $*"
 }
 function yt-dlp() {
-  nix-shell -p yt-dlp ffmpeg --run "yt-dlp $@"
+  nix-shell -p yt-dlp ffmpeg --run "yt-dlp $*"
 }
 function ncdu() {
-  nix-shell -p ncdu ffmpeg --run "ncdu --color dark -rr -x --exclude .git --exclude node_modules $@"
+  nix-shell -p ncdu ffmpeg --run "ncdu --color dark -rr -x --exclude .git --exclude node_modules $*"
 }
