d54d00d2ce3b6ef1ce4e01d9ec59d582a651f873
Jason Raimondi <jason@raimondi.us>
2025-02-05 11:26:48 -0500

    fix: needed for global pnpm packages

--- zsh/dot-zsh/04-programming.zsh
+++ zsh/dot-zsh/04-programming.zsh
@@ -1,5 +1,5 @@
-# export PNPM_HOME="$HOME/Library/pnpm"
-# case ":$PATH:" in
-#     *":$PNPM_HOME:"*) ;;
-#     *) export PATH="$PNPM_HOME:$PATH" ;;
-# esac
+export PNPM_HOME="$HOME/Library/pnpm"
+case ":$PATH:" in
+    *":$PNPM_HOME:"*) ;;
+    *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
