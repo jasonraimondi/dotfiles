c80c22d0bc5da5774723a6e10445c2f98895d5f2
Jason Raimondi <jason@raimondi.us>
2025-04-25 22:03:47 -0400

    feat: add openssl to ffmpeg, update brewfiles

--- setup.sh
+++ setup.sh
@@ -13,14 +13,14 @@ sudo -v
 bash setup-systemprefs.sh
 
 # install the more important stuff first
-brew bundle --file brew/Requirefile --no-lock
+brew bundle --file brew/Requirefile
 
 # set symlinks using stow
 bash setup-stow.sh
 
 # Install applications using homebrew & casks
 for BREWFILE in Brewfile Fontfile Caskfile Macfile; do
-  brew bundle --file "brew/$BREWFILE" --no-lock
+  brew bundle --file "brew/$BREWFILE"
 done
 
 brew cleanup
