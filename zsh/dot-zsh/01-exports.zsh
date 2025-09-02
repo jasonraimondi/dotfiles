c41b1d7913d2b538e61257686ec08459b65c9d49
Jason Raimondi <jason@raimondi.us>
2025-09-02 11:52:40 -0400

    feat: add betterauth telemetry block

--- zsh/dot-zsh/01-exports.zsh
+++ zsh/dot-zsh/01-exports.zsh
@@ -11,12 +11,11 @@ export HOMEBREW_NO_ANALYTICS=1
 # Hide homebrew hints (see `man brew`).
 export HOMEBREW_NO_ENV_HINTS=1
 
-# fixes issue when running rspec on macos when using spring
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-
-# blocks post install messages from some node_modules
 export ADBLOCK=true
+
+# disable telemetry
 export NEXT_TELEMETRY_DISABLED=1
+export BETTER_AUTH_TELEMETRY=0
 
 # docker
 export COMPOSE_DOCKER_CLI_BUILD=1
