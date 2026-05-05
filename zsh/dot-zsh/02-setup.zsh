15ee1b8ebab5841859a6f6c48142a757e1e8ea71
Jason Raimondi <jason@raimondi.us>
2026-05-05 09:42:45 -0600

    refactor(zsh): make mise default, proto opt-in via direnv

--- zsh/dot-zsh/02-setup.zsh
+++ zsh/dot-zsh/02-setup.zsh
@@ -8,23 +8,21 @@ if which atuin > /dev/null; then
   eval "$(atuin init zsh --disable-up-arrow)"
 fi
 
-# prefer mise by default, only use proto if USE_PROTO is set
-if [[ "$USE_PROTO" == 1 ]]; then
-  echo "using proto";
-  export PROTO_HOME="$HOME/.proto";
-  export PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$PATH";
-elif command -v mise > /dev/null; then
-  echo "using mise";
+if command -v direnv > /dev/null; then
+  eval "$(direnv hook zsh)"
+fi
+
+# mise is the global default; proto is opt-in per-directory via direnv
+# (see ~/Code/intelligems/.envrc). PROTO_HOME is exported so .envrc can reuse it.
+export PROTO_HOME="$HOME/.proto"
+
+if command -v mise > /dev/null; then
   eval "$(mise activate zsh)"
 fi
 
 if command -v fnox > /dev/null; then
   echo "using fnox";
   eval "$(fnox activate bash)"
-fi
-
-if command -v direnv > /dev/null; then
-  eval "$(direnv hook zsh)"
 fi
 
 if command -v plan-bender > /dev/null; then
