e1b60c8b1d122ab024490b2272f7ba4c82c8c815
Jason Raimondi <jason@raimondi.us>
2026-08-02 21:24:16 -0400

    add herdr config and claude model aliases

--- zsh/dot-zsh/03-aliases-programming.zsh
+++ zsh/dot-zsh/03-aliases-programming.zsh
@@ -1,2 +1,6 @@
 # @todo move skills dir to ~/ai
 alias skills="$HOME/Code/jason/skills/add-skill.sh"
+
+alias sonnet="claude --model sonnet --effort medium"
+alias opus="claude --model opus --effort high"
+alias fabel="claude --model fabel -effort high"
