0d2bef48627408e3359fd993855928c3ffe2b263
Jason Raimondi <jason@raimondi.us>
2026-08-27 13:20:23 -0400

    chore(zsh): init wt shell integration, silence command -v stderr

--- zsh/dot-zsh/02-setup.zsh
+++ zsh/dot-zsh/02-setup.zsh
@@ -1,14 +1,14 @@
 # homebrew computer club
-if which brew > /dev/null; then
+if which brew > /dev/null 2>&1; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # atuin shell history
-if which atuin > /dev/null; then
+if which atuin > /dev/null 2>&1; then
   eval "$(atuin init zsh --disable-up-arrow)"
 fi
 
-if command -v direnv > /dev/null; then
+if command -v direnv > /dev/null 2>&1; then
   eval "$(direnv hook zsh)"
 fi
 
@@ -16,13 +16,17 @@ fi
 # (see ~/Code/intelligems/.envrc). PROTO_HOME is exported so .envrc can reuse it.
 export PROTO_HOME="$HOME/.proto"
 
-if command -v mise > /dev/null; then
+if command -v mise > /dev/null 2>&1; then
   eval "$(mise completion zsh)"
 fi
 
-if command -v fnox > /dev/null; then
+if command -v fnox > /dev/null 2>&1; then
   echo "using fnox";
   eval "$(fnox activate bash)"
+fi
+
+if command -v wt >/dev/null 2>&1; then
+  eval "$(command wt config shell init zsh)"
 fi
 
 # if command -v plan-bender > /dev/null; then
